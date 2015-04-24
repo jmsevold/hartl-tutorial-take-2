7ee28fbab6fc7972fdbbc975482058642e3ec963
Jonathan Sevold <jmsevold@gmail.com>
2015-04-23 17:24:09 -0700

    bug fix

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
-    get signup_path
+    get sign_up_path
     assert_no_difference 'User.count' do
       post users_path, user: { name:  "",
                                email: "user@invalid",
@@ -14,7 +14,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
 
    test "valid signup information" do
-    get signup_path
+    get sign_up_path
     assert_difference 'User.count', 1 do
       post_via_redirect users_path, user: { name:  "Example User",
                                             email: "user@example.com",
